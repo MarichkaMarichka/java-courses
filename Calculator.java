2ea9fcb0bd2f59e88bfc21a9f89abc1d93285213
MariaKulakova <mkulakova923@gmail.com>
2016-01-31 17:48:22 +0200

    lesson 5

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,37 @@
+/** 
+* Class Calculator can take actions: + , pow , undo result. 
+*/
 public class Calculator{
+	/**
+	*field result - result actions of calculator;
+	*/
 	private int result;
-	public void add(int ... params){
-		for(Integer param : params)
-			this.result += param;
+	/**
+	* @param first - first number for calculation;
+	* @param second - second number for calculation;
+	* method add - adds first and second number, after adds to result;
+	*/
+	public void add(int first, int second){
+		
+			this.result += first+second;
 	}
-	
-	public void exponent(int param, int degree){
-			this.result = 0;//Math.pow(param,degree);
+	/**
+	* @param base - base of degree;
+	* @param exp - exponent;
+	* method degree - calculate:  result + base^exp;
+	*/
+	public void degree(int base, int exp){
+			//this.result = Math.pow(base,exp);
 	}
+	/**
+	* method getResult() @return result of calculation;
+	*/
 	public int getResult(){
 		return this.result;
 	}
+	/**
+	* method cleanResult() undos result; 
+	*/
 	public void cleanResult(){
 		this.result = 0 ;
 	}
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,50 +1,87 @@
 import java.util.Scanner;
+/**
+*
+*/
 public class InteractRunner{
 	public static void main(String[] arg){
+		Calculator calc = new Calculator();
 		Scanner reader = new Scanner(System.in);
 		try{
-			Calculator calc = new Calculator();
 			String exit = "no";
 			while(!exit.equals("yes")){
 				System.out.println("Enter first arg : ");
+				/** user input first number */
 				String first = reader.next();
 				System.out.println("Enter second arg : ");
+				/** user input second number */
 				String second = reader.next();
 				System.out.println("Enter '+'/'pow' : ");
+				/** user input operator for calculation */
 				String operator = reader.next();
+				
+				calculate(operator,first,second,calc);
+				
+				System.out.println("Result : "+calc.getResult());
+				System.out.println("Undo result : yes/no");
+				/** user input value for undo/not undo result */
+				String annulment = reader.next();
+				System.out.println("Exit: yes/no");
+				
+				undoOrNot(annulment,calc);
+				
+				/** user input value for exit/not exit result */
+				exit = reader.next();//out of the cycle;
+			}
+		}
+		finally{
+			reader.close();
+		}	
+	}
+		/**
+		* 
+	    * @param annulment -user's request to clean result or no(yes/no)
+		*/
+		public static void undoOrNot(String annulment,Calculator calc){
+			Boolean input = false;
+			/**
+			* cycle will run until the user will enter an correct value( yes/no );
+			*/
+			while(input.equals(false)){
+				if(annulment.equals("yes")){
+					calc.cleanResult();
+					input = true;//out of the cycle;
+				}
+				else if(annulment.equals("no")){
+					input = true;//out of the cycle;
+				}
+			}
+		}
+		/**
+		* Method calculate value of action of operator
+	    * @param operator - operator for calculation result of numbers
+		*/
+		public static void calculate(String operator,String first, String second, Calculator calc)
+			{
+				Scanner reader = new Scanner(System.in);
 				Boolean input = false;
+				/**
+				* cycle will run until the user will enter an correct operation( +, pow );
+				*/
 				while(input.equals(false)){
 					switch(operator){
 						case "+" :
 							    calc.add(Integer.valueOf(first),Integer.valueOf(second));
-								input = true;
+								input = true; //out of the cycle;
 								break;
 						case "pow" :
-								calc.exponent(Integer.valueOf(first),Integer.valueOf(second));
-								input = true;
+								//calc.exponent(Integer.valueOf(first),Integer.valueOf(second));
+								input = true; //out of the cycle;
 								break;
 						default:
 								System.out.println("Select the action : ");
 								operator = reader.next();
-								}
-							}
-				System.out.println("Result : "+calc.getResult());
-				System.out.println("Undo result : yes/no");
-				String annulment = reader.next();
-				input = false;
-				while(input.equals(false)){
-		        if(annulment.equals("yes")){
-					calc.cleanResult();
-					input = true;
+					}
 				}
-				if(annulment.equals("no")){
-					input = true;
-				}}
-				System.out.println("Exit: yes/no");
-			exit = reader.next();}
-			}finally{
-				reader.close();
 			}
 			
-		}
 	}	
